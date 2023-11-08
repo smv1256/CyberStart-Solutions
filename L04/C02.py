018b8e7e5ec96315123359774a404f218096d2d5
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-08 10:33:35 -0500

    Create C02.py

---
+++ L04/C02.py
@@ -0,0 +1,17 @@
+#
+# Fix the below script to read from each agent file found in /tmp.
+# Example agent profile would be /tmp/agent-1.txt
+# The contents of each of the agent files makes up the flag
+#
+
+import os.path
+
+count = 1
+
+for count in range(21):
+  fname = "/tmp/agent-" + str(count)+".txt"
+  if os.path.isfile(fname):
+    with open(fname, 'r') as content_file:
+      content = content_file.read()
+      print(content.rstrip())
+count += 1
