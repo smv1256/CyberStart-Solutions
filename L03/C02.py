46101041a54e3c91241bbc09ab70055cc0f500c3
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-08 18:38:44 -0500

    Update C02.py

--- L03/C02.py
+++ L03/C02.py
@@ -2,6 +2,7 @@
 #              port 9990.
 #              Your client must send "Knock, knock" to the server.
 #              Then get the response, and print it out.
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 9990))
 sock.send("Knock, knock".encode())
