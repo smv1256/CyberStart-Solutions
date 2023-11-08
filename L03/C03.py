fc91ef61856808dc45065da6f96ab3a8c96a7bcb
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-08 10:01:15 -0500

    Create C03.py

---
+++ L03/C03.py
@@ -0,0 +1,4 @@
+# CHALLENGE 1: Make a connection to: 127.0.0.1:8080/winning and print
+#              the response.
+resp = urllib.request.urlopen("http://127.0.0.1:8080/winning")
+print(resp.read())
