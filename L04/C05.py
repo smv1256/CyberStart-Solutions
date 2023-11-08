4c0bfdc05f8f7952703f0f75824623306beeb28f
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-08 18:50:32 -0500

    Create C05.py

---
+++ L04/C05.py
@@ -0,0 +1,26 @@
+#
+# Connect to alien server ('localhost', 10000)
+#
+# Then send each of these values...
+# USER
+# aliensignal
+# PASS
+# unlockserver
+# SEND
+# moonbase
+# END
+# ...and receive the response from each.
+#
+# Note: You must receive data back from the server after you send each value
+#
+
+import socket
+
+vals = ["USER", "aliensignal", "PASS", "unlockserver", "SEND", "moonbase", "END"]
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 10000))
+
+for x in vals:
+  sock.send(x.encode())
+  print(sock.recv(1024))
