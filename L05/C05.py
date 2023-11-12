797dacdebd7ab404ca6ffa9060a6225ff84f831c
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-12 17:34:16 -0500

    Create C05.py

---
+++ L05/C05.py
@@ -0,0 +1,21 @@
+#
+# The Tweet Bot API can be found at http://127.0.0.1:8082
+#
+# GET method sent to that URL:
+# ...returns basic info about API
+#
+# POST method sent to that URL, with:
+# - x-api-key:{KEY} in header
+# - user={USER} in querystring
+# - status-update={TEXT} in querystring
+# ...creates a new social media post
+
+# ez B)
+# you don't even need to do the GET request btw
+# the info is useless
+
+from urllib import parse, request
+
+data = parse.urlencode({ "user" : "tweetbotuser", "status-update" : "alientest" }).encode()
+
+print(request.urlopen(request.Request("http://127.0.0.1:8082", headers = { "x-api-key" : "tweetbotkeyv1" }, data = data)).read())
