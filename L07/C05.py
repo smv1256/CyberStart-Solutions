5aea73a962937ffe4c9be2bee558d7bcbddc7982
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-11-20 14:34:35 -0500

    Create C05.py

---
+++ L07/C05.py
@@ -0,0 +1,13 @@
+#
+# We need you to send a spoofed email.
+# Use smtp server at '127.0.0.1', port 1025.
+# Author needs to be bob-roswell-1947@ship-shape-security.com
+# Recipient needs to be zultron@cyberdarkart.com
+#
+
+import smtplib
+
+with smtplib.SMTP("127.0.0.1", 1025) as s:
+  s.sendmail("bob-roswell-1947@ship-shape-security.com", "zultron@cyberdarkart.com", "yo")
+
+  
